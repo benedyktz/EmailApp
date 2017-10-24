213b5eaa0d8bb3365da5faa23e5d4f82393222f7
Benedykt <ziolkowski.benedykt@gmail.com>
2017-10-24 21:10:41 +0200

    create outline

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,24 @@
 package emailapp;
 
 public class Email {
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	// Create a constructor with first name and last name
+	
+	// Ask for the department
+	
+	// Generate password
+	
+	// Set the mailbox capacity
+	
+	// Set the alternate Email
+	
+	// Change the password
 
 }
